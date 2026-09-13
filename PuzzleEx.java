33cf1dc2563faaf38800d03e9f350aa99fdda597
Ankur Ghosh <ankurghosh271005@gmail.com>
2026-09-13 22:56:52 +0530

    Update PuzzleEx.java

--- PuzzleEx.java
+++ PuzzleEx.java
@@ -124,5 +124,7 @@ class MyButton extends JButton {
                  
         add(panel, BorderLayout.CENTER);
         for (int i = 0; i < 4; i++) {
+             for (int j = 0; j < 3; j++) {
+
 
        
