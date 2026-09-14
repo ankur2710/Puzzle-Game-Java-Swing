93507e14d13dae80a0adb08f5bf8d629e4c3a37e
Ankur Ghosh <ankurghosh271005@gmail.com>
2026-09-14 23:39:35 +0530

    Update PuzzleEx.java

--- PuzzleEx.java
+++ PuzzleEx.java
@@ -131,6 +131,7 @@ class MyButton extends JButton {
 
                           var button = new MyButton(image);
                            button.putClientProperty("position", new Point(i, j));
+                           if (i == 3 && j == 2) {
 
 
        
