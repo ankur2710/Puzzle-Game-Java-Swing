acda3bd97356981a7af0b9b55c0e5bcc6e91cdec
Ankur Ghosh <ankurghosh271005@gmail.com>
2026-09-18 23:06:52 +0530

    Update PuzzleEx.java

--- PuzzleEx.java
+++ PuzzleEx.java
@@ -171,6 +171,7 @@ class MyButton extends JButton {
              var resizedImage = new BufferedImage(width, height, type);
            var g = resizedImage.createGraphics();
              g.drawImage(originalImage, 0, 0, width, height, null);
+              g.dispose();
     
 
 
